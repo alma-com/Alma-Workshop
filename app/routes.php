77361560d2da2f24f6ee52d3fdee3e70d85a66c5
zualex <Azubarev32@yandex.ru>
2016-08-09 08:38:50 +0300

    close site for guest

--- app/routes.php
+++ app/routes.php
@@ -22,7 +22,24 @@ Route::group([ 'prefix' => 'admin', 'namespace' => 'Controllers\Admin' ], functi
     Route::controller('users', 'UsersController');
 });
 
-Route::group([ 'namespace' => 'Controllers' ], function () {
+
+Route::group(['namespace' => 'Controllers' ], function () {
+    # Authentication and registration routes
+    Route::get('login', ['as' => 'auth.login','uses' => 'AuthController@getLogin' ]);
+    Route::post('login', 'AuthController@postLogin');
+    Route::get('login/github', [ 'as' => 'auth.login.github', 'uses' => 'AuthController@getLoginWithGithub' ]);
+    Route::get('register', ['before' => 'auth',  'as' => 'auth.register', 'uses' => 'AuthController@getRegister']);
+    Route::post('register', ['before' => 'auth', 'uses' => 'AuthController@postRegister']);
+    Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout' ]);
+
+    # Password reminder routes
+    Route::controller('password', 'RemindersController', [
+        'getRemind' => 'auth.remind',
+        'getReset'  => 'auth.reset'
+    ]);
+});
+
+Route::group(['before' => 'auth', 'namespace' => 'Controllers' ], function () {
     # Home routes
     Route::get('/', [ 'as' => 'browse.recent', 'uses' => 'BrowseController@getBrowseRecent' ]);
     Route::get('popular', [ 'as' => 'browse.popular', 'uses' => 'BrowseController@getBrowsePopular' ]);
@@ -48,20 +65,6 @@ Route::group([ 'namespace' => 'Controllers' ], function () {
     # Sitemap route
     Route::get('sitemap', 'SitemapController@getIndex');
     Route::get('sitemap.xml', 'SitemapController@getIndex');
-
-    # Authentication and registration routes
-    Route::get('login', [ 'as' => 'auth.login', 'uses' => 'AuthController@getLogin' ]);
-    Route::post('login', 'AuthController@postLogin');
-    Route::get('login/github', [ 'as' => 'auth.login.github', 'uses' => 'AuthController@getLoginWithGithub' ]);
-    Route::get('register', [ 'as' => 'auth.register', 'uses' => 'AuthController@getRegister']);
-    Route::post('register', 'AuthController@postRegister');
-    Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout' ]);
-
-    # Password reminder routes
-    Route::controller('password', 'RemindersController', [
-        'getRemind' => 'auth.remind',
-        'getReset'  => 'auth.reset'
-    ]);
 
     # User profile routes
     Route::get('user', [ 'as' => 'user.index', 'uses' => 'UserController@getIndex' ]);
